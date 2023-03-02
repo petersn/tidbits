2223e3c52d63459dcf492c7ea9fdbe299598998e
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2023-03-02 01:50:05 -0800

    Adding react template

---
+++ react-template/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App(props: {}) {
+  return <>Hello, world!</>;
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
